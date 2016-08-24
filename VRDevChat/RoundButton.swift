9d6eac24ade0a078fe931f46235e89ae0ce4efbc
Vladimir Romanov <vladromweb@gmail.com>
2016-08-24 12:23:48 -0700

    built a custom UI for the login screen
    built a custom UI for the login screen

---
+++ VRDevChat/RoundButton.swift
@@ -0,0 +1,37 @@
+//
+//  RoundButton.swift
+//  VRDevChat
+//
+//  Created by Volodymyr Romanov on 8/24/16.
+//  Copyright © 2016 Volodymyr Romanov. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
+    
+    @IBInspectable var bgColor: UIColor? {
+        didSet {
+            backgroundColor = bgColor
+        }
+    }
+}
---
+++ VRDevChat/RoundTextField.swift
@@ -0,0 +1,45 @@
+//
+//  RoundTextField.swift
+//  VRDevChat
+//
+//  Created by Volodymyr Romanov on 8/24/16.
+//  Copyright © 2016 Volodymyr Romanov. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundTextField: UITextField {
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
+    
+    @IBInspectable var bgColor: UIColor? {
+        didSet {
+            backgroundColor = bgColor
+        }
+    }
+    
+    @IBInspectable var placeholderColor: UIColor? {
+        didSet {
+            let rawString = attributedPlaceholder?.string != nil ? attributedPlaceholder!.string : ""
+            let str = NSAttributedString(string: rawString, attributes: [NSForegroundColorAttributeName: placeholderColor!])
+            attributedPlaceholder = str
+        }
+    }
+}
